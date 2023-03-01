8852df10564a6ef785c7d620ee4c92d4a943a4b1
darrenliu <darrenl2025@student.cis.edu.hk>
2023-03-01 08:48:24 +0800

    Added Song Class

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,30 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> newListeners;
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        newListeners = new ArrayList();
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+    public int howMany(ArrayList<String> input){
+        int count = 0;
+        for(String x : input){
+            if(!newListeners.contains(x.toLowerCase())){
+                newListeners.add(x.toLowerCase());
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
